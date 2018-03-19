f5de819b948ab1bb3a09200dec9aa9324e45d158
luca-lorusso <arenzano02>
2018-03-19 16:54:01 +0100

    ADD DB

--- app/src/main/java/com/lorusso/luca/smartshoplist/Category.java
+++ app/src/main/java/com/lorusso/luca/smartshoplist/Category.java
@@ -1,5 +1,7 @@
 package com.lorusso.luca.smartshoplist;
 
+import java.util.ArrayList;
+
 /**
  * Created by Luca on 15/03/2018.
  */
@@ -7,13 +9,38 @@ package com.lorusso.luca.smartshoplist;
 public class Category {
     private int idCategory;
     private String name;
+    private ArrayList<Product> p;
 
     public Category(){
 
     }
-    public Category(int idCategory, String name) {
+    public Category(int idCategory, String name, ArrayList<Product> p) {
         this.idCategory = idCategory;
+        this.name = name;
+        this.p = p;
+    }
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
+    public ArrayList<Product> getP() {
+        return p;
+    }
+
+    public void setP(ArrayList<Product> p) {
+        this.p = p;
+    }
 }
--- app/src/main/java/com/lorusso/luca/smartshoplist/DBproduct.java
+++ app/src/main/java/com/lorusso/luca/smartshoplist/DBproduct.java
@@ -1,5 +1,6 @@
 package com.lorusso.luca.smartshoplist;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,26 +10,70 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DBproduct extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "MyProduct.db";
-    public static final String CONTACTS_TABLE_NAME = "category";
-    public static final String CONTACTS_COLUMN_ID = "idCategory";
-    public static final String CONTACTS_COLUMN_NAME = "nameCategory";
+
+    public static final String DATABASE_NAME = "Data-warehouse";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String PRODUCT_TABLE = "product";
+    public static final String CATEGORY_TABLE = "category";
+
+
+    public static final String PRODUCT_COLUMN_ID = "idProduct";
+    public static final String PRODUCT_COLUMN_NAME = "nameProduct";
+    public static final String PRODUCT_COLUMN_QUANTITY = "quantity";
+    public static final String PRODUCT_COLUMN_DESCRIPTION = "description";
+
+    public static final String CATEGORY_COLUMN_ID = "idCategory";
+    public static final String CATEGORY_COLUMN_NAME = "nameCategory";
+
+
+
+    public static final String CREATE_PRODUCT_TABLE =
+            "CREATE TABLE " + PRODUCT_TABLE + "("
+                    + PRODUCT_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + PRODUCT_COLUMN_NAME + " TEXT,"
+                    + PRODUCT_COLUMN_QUANTITY + " INTEGER"
+                    + PRODUCT_COLUMN_DESCRIPTION + " TEXT"
+                    + ")";
+
+    public static final String CREATE_CATEGORY_TABLE =
+            "CREATE TABLE " + CATEGORY_TABLE + "("
+                    + CATEGORY_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + CATEGORY_COLUMN_NAME + " TEXT,"
+                    + ")";
 
     public DBproduct(Context context) {
-        super(context, DATABASE_NAME , null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         // TODO Auto-generated method stub
-        db.execSQL(
-                "create table category " +
-                        "(idCategory integer primary key, nameCategory text)"
-        );
+        db.execSQL(CREATE_PRODUCT_TABLE);
     }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // TODO Auto-generated method stub
-        db.execSQL("DROP TABLE IF EXISTS contacts");
+        db.execSQL("DROP TABLE IF EXISTS " + PRODUCT_TABLE);
         onCreate(db);
+    }
+
+    public long insertNote(String note) {
+        // get writable database as we want to write data
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        // `id` and `timestamp` will be inserted automatically.
+        // no need to add them
+        values.put(TABLE, note);
+        // insert row
+        long id = db.insert(Product.TABLE_NAME, null, values);
+
+        // close db connection
+        db.close();
+
+        // return newly inserted row id
+        return id;
     }
 }
